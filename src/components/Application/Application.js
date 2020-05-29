842f39cac69f6dc078efb08a2715de9bacad9817
Marcelo Ribeiro <me@marceloribeiro.us>
2020-05-29 04:04:03 -0700

    Simple react app repo

---
+++ src/components/Application/Application.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './application.css';
+class Application extends Component {
+render() {
+    return  <div class="Application">
+              <h1>Goodbye</h1>
+            </div>
+  }
+}
+export default Application;
